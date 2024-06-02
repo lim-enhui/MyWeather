2f403eb5c88450942ab025df4d38aa5a90e77ae6
lim-enhui <lim.enhui@outlook.com>
2024-06-02 23:28:50 +0800

    Initial Commit

---
+++ MyWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyWeather
+//
+//  Created by Lim En Hui on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Lim En Hui on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
