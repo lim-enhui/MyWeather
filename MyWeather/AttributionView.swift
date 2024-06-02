695c2a1184f84cdf1af0366904f0c8e05435bd50
lim-enhui <lim.enhui@outlook.com>
2024-06-03 00:42:22 +0800

    1. WeatherKit: Introduction and CurrentWeather Conditions

---
+++ MyWeather/AttributionView.swift
@@ -0,0 +1,18 @@
+//
+//  AttributionView.swift
+//  MyWeather
+//
+//  Created by Lim En Hui on 3/6/24.
+//
+
+import SwiftUI
+
+struct AttributionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttributionView()
+}
---
+++ MyWeather/Services/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  MyWeather
+//
+//  Created by Lim En Hui on 2/6/24.
+//
+
+import Foundation
